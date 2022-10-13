3e05c9d9006497844e8607c7760b3b151640a38e
JoE11-y <Josephedoh77@gmail.com>
2022-10-13 04:51:12 +0100

    contract improvements

--- src/contracts/giftcard_contract.py
+++ src/contracts/giftcard_contract.py
@@ -1,5 +1,6 @@
 from pyteal import *
 
+
 class GiftCard:
     class Variables:
         number = Bytes("NUMBER")
@@ -7,60 +8,81 @@ class GiftCard:
         image = Bytes("IMAGE")
         amount = Bytes("AMOUNT")
         bought = Bytes("BOUGHT")
-        address = Bytes("ADDRESS")
-        owner = Bytes("OWNER")
+        current_owner = Bytes("OWNER")
 
     class AppMethods:
         buy = Bytes("buy")
         sell = Bytes("sell")
 
+    # allow users to create a new gift card application
     def application_creation(self):
         return Seq([
-            Assert(Txn.application_args.length() == Int(5)),
-            Assert(Txn.note() == Bytes("giftcard:uv2")),
-             Assert(Btoi(Txn.application_args[3]) > Int(0)),
+            Assert(
+                # run the following checks that:
+                And(
+                    # The number of arguments attached to the transaction should be exactly 4.
+                    Txn.application_args.length() == Int(4),
+                    # The note attached to the transaction must be "giftcard:uv3".
+                    Txn.note() == Bytes("giftcard:uv3"),
+                    # The giftcard price is greater than 0
+                    Btoi(Txn.application_args[3]) > Int(0),
+                )
+            ),
             App.globalPut(self.Variables.number, Txn.application_args[0]),
             App.globalPut(self.Variables.description, Txn.application_args[1]),
             App.globalPut(self.Variables.image, Txn.application_args[2]),
-            App.globalPut(self.Variables.amount, Btoi(Txn.application_args[3])),
+            App.globalPut(self.Variables.amount,
+                          Btoi(Txn.application_args[3])),
             App.globalPut(self.Variables.bought, Int(0)),
-            App.globalPut(self.Variables.address, Global.creator_address()),
-            App.globalPut(self.Variables.owner, Txn.application_args[4]),
+            App.globalPut(self.Variables.current_owner, Txn.accounts[0]),
             Approve()
         ])
 
+    # allow users to buy a gift card that hasn't been bought yet
     def buyCard(self):
         return Seq([
             Assert(
+                # run the following checks that:
                 And(
+                    # The transaction group is made up of 2 transactions
                     Global.group_size() == Int(2),
-                    Txn.application_args.length() == Int(2),
-                    App.globalGet(self.Variables.bought) == Int(0)
+                    # The buyCard txn is made ahead of the payment transaction
+                    Txn.group_index() == Int(0),
+                    # The item hasn't been bought
+                    App.globalGet(self.Variables.bought) == Int(0),
+                    # The sender of transaction is not the giftcard owner
+                    App.globalGet(
+                        self.Variables.current_owner) != Txn.sender(),
                 ),
             ),
             Assert(
+                # checks for the payment transaction
                 And(
                     Gtxn[1].type_enum() == TxnType.Payment,
                     Gtxn[1].receiver() == App.globalGet(
-                        self.Variables.address),
+                        self.Variables.current_owner),
                     Gtxn[1].amount() == App.globalGet(self.Variables.amount),
                     Gtxn[1].sender() == Gtxn[0].sender(),
                 )
             ),
 
-            App.globalPut(self.Variables.owner, Txn.application_args[1]),
-            App.globalPut(self.Variables.address, Gtxn[1].sender()),
-            App.globalPut(self.Variables.amount, App.globalGet(self.Variables.amount) * Int(2)),
+            App.globalPut(self.Variables.current_owner, Txn.accounts[0]),
+            App.globalPut(self.Variables.amount, App.globalGet(
+                self.Variables.amount) * Int(2)),
             App.globalPut(self.Variables.bought, Int(1)),
             Approve()
         ])
 
+    # allow gift cards' owners to sell their gift cards
     def sellGiftCard(self):
         Assert(
+            # run the following checks that:
             And(
-                Txn.application_args.length() == Int(2),
+                # The gift card is already bought
+                App.globalGet(self.Variables.bought) == Int(1),
+                # The sender is the gift card's owner
                 App.globalGet(
-                    self.Variables.owner) == Txn.application_args[1]
+                    self.Variables.current_owner) == Txn.sender(),
             ),
         )
 
@@ -86,6 +108,3 @@ class GiftCard:
 
     def clear_program(self):
         return Return(Int(1))
-
-
-        
